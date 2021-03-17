4e5503929c5aaadc2a6a6645c3a59aa839d4fc4d
Cédric Dufour <cedric.dufour@ced-network.net>
2021-03-17 22:30:51 +0100

    - FIX: Milliseconds being ingored in time units

--- src/units/CUnitTime.cpp
+++ src/units/CUnitTime.cpp
@@ -71,7 +71,7 @@ CUnitTime::EUnit CUnitTime::fromCode( const QString& _rqString )
 double CUnitTime::toValue( double _fdValue, CUnitTimeZone::EUnit _eUnitTimeZone )
 {
   QDateTime __qDateTime = QDateTime::fromTime_t( _fdValue );
-  __qDateTime.addMSecs( 1000.0*fmod( _fdValue, 1.0 ) + 0.5 );
+  __qDateTime = __qDateTime.addMSecs( 1000.0*fmod( _fdValue, 1.0 ) + 0.5 );
   QTime __qTime = _eUnitTimeZone == CUnitTimeZone::UTC ? __qDateTime.toUTC().time() : __qDateTime.toLocalTime().time();
   return 3600.0*__qTime.hour() + 60.0*__qTime.minute() + __qTime.second() + __qTime.msec()/1000.0;
 }
@@ -79,7 +79,7 @@ double CUnitTime::toValue( double _fdValue, CUnitTimeZone::EUnit _eUnitTimeZone 
 QString CUnitTime::toString( double _fdValue, CUnitTimeZone::EUnit _eUnitTimeZone, CUnitTime::EUnit _eUnit, int _iPrecision )
 {
   QDateTime __qDateTime = QDateTime::fromTime_t( _fdValue );
-  __qDateTime.addMSecs( 1000.0*fmod( _fdValue, 1.0 ) + 0.5 );
+  __qDateTime = __qDateTime.addMSecs( 1000.0*fmod( _fdValue, 1.0 ) + 0.5 );
   QTime __qTime = _eUnitTimeZone == CUnitTimeZone::UTC ? __qDateTime.toUTC().time() : __qDateTime.toLocalTime().time();
   switch( _eUnit )
   {
